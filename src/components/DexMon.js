3fc86f114d8935730b273fdf942d9ffaea57872f
Ash <ashley.davis@digital8.com.au>
2020-05-16 23:50:55 +1000

    some tinkering

---
+++ src/components/DexMon.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {Placeholder} from 'semantic-ui-react'
+import styled, {withTheme} from 'styled-components'
+import Row from './Row'
+
+const _DexMonWrapper = styled.div`
+  grid-column-start: auto;
+  grid-column-end: auto;
+  grid-row-start: auto;
+  grid-row-end: auto;
+  height: 160px;
+  width: 120px;
+  border: 1px solid #ccc;
+`
+
+const _DexMon = styled.div`
+  padding: 20px;
+  height: 100%;
+`
+
+const DexMon = ({mon = {fetching: true}, ...props}) => (
+  <_DexMonWrapper>
+    <_DexMon>
+      <div>
+        <Row jc="center">
+          {mon.fetching && (
+            <Placeholder style={{height: 60, width: 60}}>
+              <Placeholder.Image square />
+            </Placeholder>
+          )}
+        </Row>
+        <div>#{(props.index + 1).toPokedex()}</div>
+        <div>{mon.name}</div>
+        {mon.fetching ? (
+          <Placeholder>
+            <Placeholder.Paragraph>
+              <Placeholder.Line length="short" />
+            </Placeholder.Paragraph>
+          </Placeholder>
+        ) : (
+          <div>Type Tag</div>
+        )}
+      </div>
+    </_DexMon>
+  </_DexMonWrapper>
+)
+
+export default withTheme(DexMon)
--- src/pages/Dex.js
+++ src/pages/Dex.js
@@ -1,13 +1,118 @@
+import axios from 'axios'
 import React, {Component} from 'react'
+import InfiniteScroll from 'react-infinite-scroller'
+import styled, {withTheme} from 'styled-components'
+import DexMon from '../components/DexMon'
+import Row from '../components/Row'
 
 class Dex extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      take: 12,
+      start: 0,
+
+      pokedexList: [],
+      rawRes: '',
+
+      loading: false,
+    }
   }
+
+  getTake() {
+    const {pokedexList} = this.state
+    return Math.min(151 - pokedexList.length, 12)
+  }
+
+  getTheGuys = async () => {
+    const {loading, take, start, pokedexList} = this.state
+    if (loading || pokedexList.length >= 151) return
+    this.setState({loading: true})
+    const getTakeAmount = this.getTake()
+    // console.log('@@fetching', getTakeAmount, start)
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/?limit=${getTakeAmount}&offset=${start}`
+    axios({
+      method: 'get',
+      url: apiUrl,
+    })
+      .then((res) => {
+        const {pokedexList} = this.state
+        const newlist =
+          res?.data?.results.map((e) => {
+            return {...e, fetching: true}
+          }) || []
+        this.setState(
+          {
+            start: start + take,
+            pokedexList: [...pokedexList, ...newlist],
+            rawRes: JSON.stringify(res),
+          },
+          () => this.setState({loading: false})
+        )
+        return newlist
+      })
+      .then((theMons) => {
+        console.log('@@fetched mons', theMons)
+        this.fetchMonData([...theMons])
+      })
+      .catch((err) => {
+        console.log(err)
+        console.log('@@error')
+        this.setState({loading: false})
+      })
+  }
+
+  fetchMonData = async (theMons) => {
+    // setTimeout(() => {
+    // }, timeout);
+  }
+
   render() {
-    return <div>Dex</div>
+    const {rawRes, pokedexList} = this.state
+    return (
+      <div
+        style={{minHeight: '100vh', overflow: 'auto'}}
+        ref={(ref) => (this.scrollParentRef = ref)}
+      >
+        <div>Dex</div>
+
+        <InfiniteScroll
+          loadMore={this.getTheGuys}
+          hasMore={pokedexList.length < 151}
+          initialLoad
+          loader={
+            <div className="loader" key={0}>
+              Loading ...
+            </div>
+          }
+          threshold={100}
+          useWindow={true}
+        >
+          <Row>
+            {/* <div>Left</div> */}
+
+            <_DexContent>
+              {pokedexList.map((e, i) => (
+                <DexMon key={i} index={i} mon={e} />
+              ))}
+            </_DexContent>
+            {/* <div>Right</div> */}
+          </Row>
+        </InfiniteScroll>
+      </div>
+    )
   }
 }
 
-export default Dex
+const _DexContent = styled.div`
+  display: grid;
+  grid-template-columns: 120px 120px 120px;
+  column-gap: 20px;
+  row-gap: 20px;
+
+  @media only screen and (max-width: 400px) {
+    grid-template-columns: 120px 120px;
+  }
+`
+
+export default withTheme(Dex)
